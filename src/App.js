711b9598823f2d57ac1f8e4088fc0766b70660bd
Manish Suyal <manishsuyal2099@Manishs-MacBook-Air.local>
2023-11-28 17:35:57 +0530

    product page and item ui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Navbar } from './Components/Navbar/Navbar';
 import { BrowserRouter, Route , Routes } from 'react-router-dom';
 import { ShopCategory } from './Pages/ShopCategory';
 import { Shop } from './Pages/Shop';
-import { Product } from './Pages/Product';
+import Product from './Pages/Product';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Cart } from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
@@ -21,7 +21,7 @@ function App() {
 <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>} />
 <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>} />
 <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>} />
-<Route path="./product" element={<Product/>}>
+<Route path='/product' element={<Product/>}>
   <Route path=':productId' element={<Product/>}/> 
 </Route>
 <Route path='/cart' element={<Cart/>}/> 
